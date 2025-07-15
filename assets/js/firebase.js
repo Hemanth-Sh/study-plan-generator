d2779a8c6d9d328b7de345dfc1526947156dfe84
Hemanth-Sh <shemanth749@gmail.com>
2025-07-15 11:43:40 +0530

    Added frontend UI

---
+++ assets/js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+     const firebaseConfig = {
+            apiKey: "AIzaSyBCsvKjdNClB_mntMuaTVU7XxeWtqPOeu0",
+            authDomain: "techtitans-studyplan.firebaseapp.com",
+            projectId: "techtitans-studyplan",
+            storageBucket: "techtitans-studyplan.appspot.com",
+            messagingSenderId: "704123994356",
+            appId: "1:704123994356:web:6ccffcb66fd846f4151ea1"
+        };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
